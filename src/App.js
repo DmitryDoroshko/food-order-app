d04a99722fd9834f5727930b181f3b2a9f43a283
DmitryDoroshko <dmitry.doroshko.99@gmail.com>
2022-01-15 13:20:53 +0200

    Cart structure done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import "./index.css";
 import Header from './components/Layout/Header/Header';
+import Meals from './components/Meals/Meals/Meals';
+import Cart from './components/Cart/Cart/Cart';
 
 function App() {
     return (
-        <Header />
+        <>
+            <Cart />
+            <Header />
+            <main>
+                <Meals />
+            </main>
+        </>
     );
 }
 

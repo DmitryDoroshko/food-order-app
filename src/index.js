bfc7d220e53deef19c8342792c2a1752b23413d4
DmitryDoroshko <dmitry.doroshko.99@gmail.com>
2022-01-28 15:08:42 +0200

    CartForm overflow fixed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import CartProvider from './store/CartProvider';
 
-
 ReactDOM.render(<CartProvider><App /></CartProvider>, document.getElementById('root'));
 
